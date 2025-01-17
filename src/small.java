0e97cc537c8d74f2912dfb7634fcc8dd5b1c1dc0
Mayank16092003 <mayankaa81@gmail.com>
2025-01-17 16:08:33 +0530

    small no.

--- src/small.java
+++ src/small.java
@@ -1,18 +1,15 @@
 import java.util.Scanner;
-public class smallest {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+public class small{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
         int a=sc.nextInt();
         int b=sc.nextInt();
         int c=sc.nextInt();
-        if(a>b&&a>c){
+        if(a<b&&a<c)
             System.out.println(a);
-        }
-        else if(b>a&&b>c){
+        else if(b<a&&b<c)
             System.out.println(b);
-        }
-        else if(c>a&&c>b){
+        else
             System.out.println(c);
-        }
     }
 }
